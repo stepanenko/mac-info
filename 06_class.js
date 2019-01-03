0a233d97c463708c1b3bb13d546aa2014cfbcb54
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-03 18:37:25 +0200

    class

--- 06_class.js
+++ 06_class.js
@@ -1,4 +1,16 @@
 
 class UserCreator {
-  constructor(name, score) {}
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  increment() {
+    this.score++;
+  }
 }
+
+const henry = new UserCreator('Henry', 11);
+henry.increment();
+
+console.log(henry);
