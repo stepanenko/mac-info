6fe4e57ae3e1fca5b0c3091014fdab86ea5b3185
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2022-03-13 20:09:24 +0200

    cleaned

--- 07_func_prototype.js
+++
@@ -1,9 +0,0 @@
-
-function multiplyBy2(num) {
-  return num*2;
-}
-
-multiplyBy2.toString(); // function multiplyBy2(num) {return num*2;}
-let n = multiplyBy2.hasOwnProperty('num');
-
-console.log(n);
